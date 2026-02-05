80792f16091d7c64b2d944e7ca680f5dbdf6800c
vlasio <155398464+vlasio@users.noreply.github.com>
2026-02-05 10:17:30 +0000

    func task guards

--- lab2-functionalPrimer/haskell/func_as_arg.hs
+++ lab2-functionalPrimer/haskell/func_as_arg.hs
@@ -2,8 +2,11 @@
 inpFunc a b = [a..b] 
 
 --Define applicatorFunc
-applicatorFunc inpFunc a b s = if s=='s' then sum (inpFunc a b) else (sum (inpFunc a b)) / (b-a+1)
+--applicatorFunc inpFunc a b s = if s=='s' then sum (inpFunc a b) else (sum (inpFunc a b)) / (b-a+1)
+applicatorFunc a b s
+    | s == 's' = sum (inpFunc a b)
+    | otherwise = (sum (inpFunc a b)) / (b - a + 1)
 
 main = do
-    let result = applicatorFunc inpFunc 1 10 's' --Call applicatorFunc with inpFunc and 'a' as args
+    let result = applicatorFunc 1 10 'd'
     putStrLn("sum = " ++ show(result))
